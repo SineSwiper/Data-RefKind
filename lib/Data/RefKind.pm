c95880b24e15805564cb3355713e6118964874c4
Brendan Byrd <Perl@ResonatorSoft.org>
2012-07-18 09:37:37 -0400

    Initial commit

---
+++ lib/Data/RefKind.pm
@@ -0,0 +1,36 @@
+package Data::RefKind;
+
+# VERSION
+# ABSTRACT: ---insert abstract here---
+
+use sanity;
+
+### INSERT CODE HERE ###
+
+42;
+
+__END__
+
+=begin wikidoc
+
+= SYNOPSIS
+ 
+   # code
+ 
+= DESCRIPTION
+ 
+Insert description here...
+
+= CAVEATS
+
+Bad stuff...
+
+= SEE ALSO
+
+Other modules...
+
+= ACKNOWLEDGEMENTS
+
+Thanks and stuff...
+
+=end wikidoc
